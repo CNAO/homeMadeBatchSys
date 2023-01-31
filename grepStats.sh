9a448d603b4a4b3901f68b885b6d034884e19fdc
Alessio Mereghetti <amereghe@Ubuntu-20.04-LTS>
2023-01-31 11:28:02 +0100

    removing grepStats.sh, since the funcionality has been merged into spawn.sh

--- grepStats.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-myCase=$1
-myUnit=1E6
-stats=`grep -h 'Total number of primaries run' ${myCase}/run_*/*.out | awk -v unit=${myUnit}  '{tot=tot+$6}END{print (tot/unit)}'`
-
-echo "stats for ${myCase}: ${stats} x${myUnit}"
