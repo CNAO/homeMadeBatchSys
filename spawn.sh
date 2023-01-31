a4f3d1b81ad84b50da553dab95af5503d3621242
Alessio Mereghetti <amereghe@Ubuntu-20.04-LTS>
2023-01-31 10:33:37 +0100

    adding merge action and options

--- spawn.sh
+++ spawn.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
 caseDir=""
-origDir=.
 inputFile=""
 jobFile=job_FLUKA.sh
 nPrims=""
+origDir=.
+scorings="RESNUCLE,USRBIN,USRBDX,USRTRACK"
 seedMin=1
 seedMax=10
+wherePST="run_%05i"
+whereM="run_?????"
 # what to do
 lPrepare=false
 lSubmit=false
 lStop=false
+lMerge=false
 lClean=false
 # hand-made queueing system
 lQueue=true
@@ -25,9 +29,9 @@ export LC_NUMERIC="en_US.UTF-8"
 # log terminal line command
 echo "`date +"[%Y-%m-%d %H:%M:%S]"` $0 $*" >> ${logFile}
 
-# =================================================================================
+# ==============================================================================
 # FUNCTIONs
-# =================================================================================
+# ==============================================================================
 
 die() {
   echo >&2 "$1"
@@ -62,11 +66,18 @@ cat <<EOF
                     -c <caseDir>   (mandatory)
 		    -i <inputFile> (mandatory)
 
-		    example: /mnt/DATA/homeMadeBatchSys/spawn.sh -C -c P_W -i XPRcolli.inp
+        example: /mnt/DATA/homeMadeBatchSys/spawn.sh -C -c P_W -i XPRcolli.inp
+
+       -M  merge:   to merge results saved in binary files.
+                    available options:
+                    -c <caseDir>   (mandatory)
+		    -i <inputFile> (mandatory)
+                    -s <scorings>  (optional)
+                    -w <where>     (optional)
 
        -H  help:    to print this help
 
-       	   	    example: /mnt/DATA/homeMadeBatchSys/spawn.sh -H
+        example: /mnt/DATA/homeMadeBatchSys/spawn.sh -H
 
        -P  prepare: to set up study folder, i.e. it creates the study folder,
                       with a ``master copy'' of the <inputFile> and <jobFile>,
@@ -121,6 +132,19 @@ cat <<EOF
        -p <nPrims>         number of primaries
        	  		   --> NO defaults!
 
+       -s <scorings>       FLUKA scoring detectors (cards)
+       	  		   --> default: ${scorings[@]};
+
+       -w <where>          subfolders of the study case
+       	  		   --> default for actions P,S,T: ${wherePST};
+                               format of subfolder names for regexp substitution
+       	  		   --> default for actions M: ${whereM};
+                               format of subfolder names for grepping/listing
+        NOTA BENE: given the different uses of this option for the actions, it 
+                   is recommended to use defaults; otherwise, please consider
+                   to perform a single action per script call or to merge all
+                   together the options with the same use of this options.
+                             
 EOF
 }
 
@@ -129,7 +153,7 @@ EOF
 # ==============================================================================
 
 # get options
-while getopts  ":Cc:Hi:j:m:n:o:Pp:ST" opt ; do
+while getopts  ":Cc:Hi:j:Mm:n:o:Pp:Ss:Tw:" opt ; do
   case $opt in
     C)
       lClean=true
@@ -146,6 +170,9 @@ while getopts  ":Cc:Hi:j:m:n:o:Pp:ST" opt ; do
       ;;
     j)
       jobFile=$OPTARG
+      ;;
+    M)
+      lMerge=true
       ;;
     m)
       seedMin=$OPTARG
@@ -165,8 +192,15 @@ while getopts  ":Cc:Hi:j:m:n:o:Pp:ST" opt ; do
     S)
       lSubmit=true
       ;;
+    s)
+      myScorings=$OPTARG
+      if [ "${myScorings}"!="all" ] ; then scorings=${myScorings} ; fi
+      ;;
     T)
       lStop=true
+      ;;
+    w)
+      where=$OPTARG
       ;;
     \?)
       die "Invalid option: -$OPTARG"
@@ -188,6 +222,11 @@ if ${lPrepare} ; then
 fi
 # if ${lSubmit} ; then
 # fi
+if ${lMerge} ; then
+    # mandatory options are there
+    if [ -z "${inputFile}" ] ; then die ".inp file NOT declared!" ; fi
+    if [ ${#scorings[@]} -eq 0 ] ; then die "no scorings specified!" ; fi
+fi
 if ${lClean} ; then
     # mandatory options are there
     if [ -z "${inputFile}" ] ; then die ".inp file NOT declared!" ; fi
@@ -216,11 +255,12 @@ if ${lPrepare} ; then
     cd ${origDir}
     cp ${inputFile} ${jobFile} ${currDir}/${caseDir}
     cd - > /dev/null 2>&1
+    if [ -z "${where}" ] ; then where=${wherePST} ; fi
     # final steps of preparation (a folder per seed)
     cd ${caseDir}
     for ((iSeed=${seedMin}; iSeed<=${seedMax}; iSeed++ )) ; do 
         echo " ...preparing seed ${iSeed}..."
-        dirNum=`printf "run_%05i" "${iSeed}"`
+        dirNum=`printf "${where}" "${iSeed}"`
         if [ -d ${dirNum} ] ; then
             echo " ...folder ${dirNum} already exists: recreating it!"
             rm -rf ${dirNum}
@@ -237,9 +277,10 @@ fi
 
 if ${lSubmit} ; then
     echo " submitting jobs of study ${caseDir} ..."
+    if [ -z "${where}" ] ; then where=${wherePST} ; fi
     for ((iSeed=${seedMin}; iSeed<=${seedMax}; iSeed++ )) ; do
         echo " ...submitting seed ${iSeed}..."
-        dirNum=`printf "run_%05i" "${iSeed}"`
+        dirNum=`printf "${where}" "${iSeed}"`
         if ${lQueue} ; then
             currJobFile=job_${caseDir}_${dirNum}_`date "+%Y-%m-%d_%H-%M-%S"`.sh
             cat > ${currJobFile} <<EOF
@@ -264,10 +305,11 @@ if ${lStop} ; then
         echo " ...study folder ${caseDir} does not exists! is it spelled correctly?"
         exit 1
     fi
+    if [ -z "${where}" ] ; then where=${wherePST} ; fi
     # touch rfluka.stop in all the fluka_* folders
     cd ${caseDir}
     for ((iSeed=${seedMin}; iSeed<=${seedMax}; iSeed++ )) ; do
-        dirNum=`printf "run_%05i" "${iSeed}"`
+        dirNum=`printf "${where}" "${iSeed}"`
         flukaFolders=`\ls -1d ${dirNum}/fluka*/`
         if [[ "${flukaFolders}" == "" ]] ; then
             echo " ...no FLUKA runs to stop for seed ${iSeed}!"
@@ -279,6 +321,66 @@ if ${lStop} ; then
             for flukaFolder in ${flukaFolders[@]} ; do
                 echo " ...stopping run in folder ${flukaFolder} ..."
                 touch ${flukaFolder}/rfluka.stop
+            done
+        fi
+    done
+    cd - > /dev/null 2>&1
+fi
+
+if ${lMerge} ; then
+    echo " merging binary result files of study ${caseDir} ..."
+    cd ${caseDir}
+    if [ -z "${where}" ] ; then
+        where=${whereM}
+    elif [ "${where}" == "${wherePST}"] ; then
+        where=${whereM}
+    fi
+    for myScor in ${scorings[@]} ; do
+        case ${myScor}  in
+            RESNUCLE)
+                extension="rnc"
+                exeMerge="usrsuw"
+                myCol=3
+                ;;
+            USRBDX)
+                extension="bnx"
+                exeMerge="usxsuw"
+                myCol=4
+                ;;
+            USRBIN)
+                extension="bnn"
+                exeMerge="usbsuw"
+                myCol=4
+                ;;
+            USRTRACK)
+                extension="trk"
+                exeMerge="ustsuw"
+                myCol=4
+                ;;
+            USRYIELD)
+                extension="yie"
+                exeMerge="usysuw"
+                myCol=4
+                ;;
+            *)
+                echo "...don't know how to process ${myScor} detectors! skipping..."
+                continue
+        esac
+        echo "checking presence of ${myScor} cards in ${inputFile} file..."
+        units=`grep ${myScor} ${inputFile} | grep -v -e DCYSCORE -e AUXSCORE | awk -v myCol=${myCol} '{un=-\$myCol; if (20<un && un<100) {print (un)}}' | sort -g | uniq`
+        if [[ "${units}" == "" ]] ; then
+            echo "...no cards found!"
+            continue
+        else
+            units=( ${units} )
+            echo "...found ${#units[@]} ${myScor} cards: processing..."
+            for myUnit in ${units[@]} ; do
+                echo " merging ${myScor} on unit ${myUnit} ..."
+                ls -1 ${where}/*${myUnit} > ${myUnit}.txt
+                echo "" >> ${myUnit}.txt
+                echo "${inputFile%.inp}_${myUnit}.${extension}" >> ${myUnit}.txt
+                ${FLUKA}/flutil/${exeMerge} < ${myUnit}.txt > ${myUnit}.log 2>&1
+                rm ${myUnit}.*            
             done
         fi
     done
